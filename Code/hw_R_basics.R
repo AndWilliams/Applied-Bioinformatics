db4c84df8e9192eed51dd68a62c27cebe346110c
Andrew Williams <awilliams4@students.stonehill.edu>
2020-01-22 09:42:09 -0500

    Homework

---
+++ Code/hw_R_basics.R
@@ -0,0 +1,7 @@
+x=seq(1:10)
+print(x)
+x*2 #Multiplied everything inside the vector with a factor of 2
+x-1#subtracted 1 from all the numbers but as im noticing with the math operators the vector doesnt really change it just shows you what the answer is without permanently changing the vector
+mean(x)#Averages numbers inside the vector
+max(x)#finds the largest number in the vector
+min(x)#finds the smallest number in the vector
